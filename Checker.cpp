e6325d8c12d1b46fd875bf2e21e2c02e9f1b4fa5
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-06-26 11:14:30 +0530

    Update Checker _2.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,20 +2,21 @@
 #include <iostream>
 using namespace std;
 
-
-bool checkInRange(float value, float min, float max, const char* message) {
-  if (value < min || value > max) {
-    cout << message << endl;
+// A pure function which checks if input is out of range and prints error message
+bool checkInRange(float value, float min, float max, const char* error) {
+  if (value < min || value > max) { 
+    cout << error ;//print error message
     return false;
   }
   return true;
 }
 
+//conditional statements are removed and replaced by a functional call to a pure function to reduce CCN and to follow paradigm
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
   bool temperatureIsOk = checkInRange(temperature, 0, 45, "Temperature out of range!");
   bool socIsOk = checkInRange(soc, 20, 80, "State of Charge out of range!");
   bool chargeRateIsOk = checkInRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
-  return temperatureIsOk && socIsOk && chargeRateIsOk;
+  return temperatureIsOk && socIsOk && chargeRateIsOk; 
 }
 
 int main() {
