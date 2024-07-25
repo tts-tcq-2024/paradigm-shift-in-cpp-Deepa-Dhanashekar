855d7699c0965ebdb8666bd326e646f3d2e3d5f4
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-25 16:42:08 +0530

    Update battery_check.h

--- battery_check.h
+++ battery_check.h
@@ -1,35 +1,15 @@
 #ifndef BATTERY_CHECK_H
 #define BATTERY_CHECK_H
-
+ 
 #include <map>
 #include <string>
-
+ 
 enum Language { ENGLISH, GERMAN };
-
+ 
 extern Language currentLanguage;
-
-enum BreachType {
-  NORMAL,
-  LOW_BREACH,
-  HIGH_BREACH,
-  LOW_WARNING,
-  HIGH_WARNING
-};
-
-struct ParameterRange {
-  float min;
-  float max;
-  BreachType lowBreach;
-  BreachType highBreach;
-};
-
-extern const ParameterRange temperatureRange;
-extern const ParameterRange socRange;
-extern const ParameterRange chargeRateRange;
-
-const char* getErrorMessage(BreachType breachType);
-BreachType classifyRangeBreach(float value, const ParameterRange& range);
+ 
+const char* getErrorMessage(const char* errorKey);
+bool checkInRange(float value, float min, float max, const char* errorKey);
 bool batteryIsOk(float temperature, float soc, float chargeRate);
-void printBreachMessage(BreachType breachType);
-
+ 
 #endif // BATTERY_CHECK_H
