88e36e950146ee9012fe9587ccc1565e2de383ed
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-25 16:42:52 +0530

    Update and rename main.cpp to test_battery_check.cpp

--- test_battery_check.cpp
+++ test_battery_check.cpp
@@ -1,14 +1,21 @@
 #include "battery_check.h"
 #include <cassert>
-
-int main() {
+#include <iostream>
+ 
+void testBatteryCheck() {
+  // Test in English
   currentLanguage = ENGLISH;
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
-  
+ 
+  // Test in German
   currentLanguage = GERMAN;
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
-
+  cout << "All tests passed!" << endl;
+}
+ 
+int main() {
+  testBatteryCheck();
   return 0;
 }
