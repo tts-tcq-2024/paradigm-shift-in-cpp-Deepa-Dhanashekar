59f21a237bc829046d7759dc7742ca2c5e8b4d08
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-25 14:49:44 +0530

    Update and rename Checker.cpp to battery_Checker.cpp

---
+++ battery_Checker.cpp
@@ -0,0 +1,67 @@
+#include "battery_check.h"
+#include <iostream>
+
+using namespace std;
+
+Language currentLanguage = ENGLISH;
+
+const ParameterRange temperatureRange = {0, 45, LOW_BREACH, HIGH_BREACH};
+const ParameterRange socRange = {20, 80, LOW_BREACH, HIGH_BREACH};
+const ParameterRange chargeRateRange = {0, 0.8, NORMAL, HIGH_BREACH};
+
+const char* getErrorMessage(BreachType breachType) {
+  static map<BreachType, map<Language, string>> errorMessages = {
+    {LOW_BREACH, {
+      {ENGLISH, "Value is below the minimum limit!"},
+      {GERMAN, "Wert liegt unter dem Mindestwert!"}
+    }},
+    {HIGH_BREACH, {
+      {ENGLISH, "Value is above the maximum limit!"},
+      {GERMAN, "Wert liegt über dem Höchstwert!"}
+    }},
+    {LOW_WARNING, {
+      {ENGLISH, "Value is approaching the minimum limit!"},
+      {GERMAN, "Wert nähert sich dem Mindestwert!"}
+    }},
+    {HIGH_WARNING, {
+      {ENGLISH, "Value is approaching the maximum limit!"},
+      {GERMAN, "Wert nähert sich dem Höchstwert!"}
+    }},
+    {NORMAL, {
+      {ENGLISH, "Value is within the normal range."},
+      {GERMAN, "Wert liegt im normalen Bereich."}
+    }}
+  };
+  return errorMessages[breachType][currentLanguage].c_str();
+}
+
+BreachType classifyRangeBreach(float value, const ParameterRange& range) {
+  if (value < range.min) {
+    return range.lowBreach;
+  } else if (value > range.max) {
+    return range.highBreach;
+  } else if (value < range.min + 5) { // Assuming a warning range of 5 units
+    return LOW_WARNING;
+  } else if (value > range.max - 5) { // Assuming a warning range of 5 units
+    return HIGH_WARNING;
+  }
+  return NORMAL;
+}
+
+void printBreachMessage(BreachType breachType) {
+  if (breachType != NORMAL) {
+    cout << getErrorMessage(breachType) << endl;
+  }
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+  BreachType temperatureBreach = classifyRangeBreach(temperature, temperatureRange);
+  BreachType socBreach = classifyRangeBreach(soc, socRange);
+  BreachType chargeRateBreach = classifyRangeBreach(chargeRate, chargeRateRange);
+
+  printBreachMessage(temperatureBreach);
+  printBreachMessage(socBreach);
+  printBreachMessage(chargeRateBreach);
+
+  return temperatureBreach == NORMAL && socBreach == NORMAL && chargeRateBreach == NORMAL;
+}
