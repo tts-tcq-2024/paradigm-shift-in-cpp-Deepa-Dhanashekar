b5f20755e0aebb132d6b04dcb31e96a63f23a2f0
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-25 15:08:09 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
 #include "battery_check.h"
 #include <cassert>
 
-int main() {
+void testBatteryParameterRanges() {
+  // Normal cases
+  assert(batteryIsOk(25, 70, 0.7) == true);
+  assert(batteryIsOk(0, 20, 0.0) == true);
+  assert(batteryIsOk(45, 80, 0.8) == true);
+
+  // Breach cases
+  assert(batteryIsOk(-1, 70, 0.7) == false); // Temperature below min
+  assert(batteryIsOk(25, 19, 0.7) == false); // SOC below min
+  assert(batteryIsOk(25, 70, 0.9) == false); // Charge rate above max
+  assert(batteryIsOk(50, 85, 0) == false); // Temperature and SOC above max
+
+  // Warning cases
+  assert(batteryIsOk(2, 70, 0.7) == true); // Temperature near min
+  assert(batteryIsOk(25, 21, 0.7) == true); // SOC near min
+  assert(batteryIsOk(25, 70, 0.78) == true); // Charge rate near max
+  assert(batteryIsOk(44, 79, 0.7) == true); // Temperature and SOC near max
+}
+
+void testBatteryCheck() {
+  // Test in English
   currentLanguage = ENGLISH;
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
-  
+  testBatteryParameterRanges();
+
+  // Test in German
   currentLanguage = GERMAN;
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+  testBatteryParameterRanges();
+}
+
+int main() {
+  testBatteryCheck();
 
   return 0;
 }
