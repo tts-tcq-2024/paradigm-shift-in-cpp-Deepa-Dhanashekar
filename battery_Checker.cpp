b9ad056b93d8ef24853e4247363e3cd3960b2b4b
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-25 16:41:43 +0530

    Update battery_Checker.cpp

--- battery_Checker.cpp
+++ battery_Checker.cpp
@@ -1,75 +1,39 @@
 #include "battery_check.h"
 #include <iostream>
-
+ 
 using namespace std;
-
+ 
 Language currentLanguage = ENGLISH;
-
-const ParameterRange temperatureRange = {0, 45, LOW_BREACH, HIGH_BREACH};
-const ParameterRange socRange = {20, 80, LOW_BREACH, HIGH_BREACH};
-const ParameterRange chargeRateRange = {0, 0.8, NORMAL, HIGH_BREACH};
-
-const char* getErrorMessage(BreachType breachType) {
-  static map<BreachType, map<Language, string>> errorMessages = {
-    {LOW_BREACH, {
-      {ENGLISH, "Value is below the minimum limit!"},
-      {GERMAN, "Wert liegt unter dem Mindestwert!"}
+ 
+const char* getErrorMessage(const char* errorKey) {
+  static map<string, map<Language, string>> errorMessages = {
+    {"Temperature out of range!", {
+      {ENGLISH, "Temperature out of range!"},
+      {GERMAN, "Temperatur außerhalb des zulässigen Bereichs!"}
     }},
-    {HIGH_BREACH, {
-      {ENGLISH, "Value is above the maximum limit!"},
-      {GERMAN, "Wert liegt über dem Höchstwert!"}
+    {"State of Charge out of range!", {
+      {ENGLISH, "State of Charge out of range!"},
+      {GERMAN, "Ladezustand außerhalb des zulässigen Bereichs!"}
     }},
-    {LOW_WARNING, {
-      {ENGLISH, "Value is approaching the minimum limit!"},
-      {GERMAN, "Wert nähert sich dem Mindestwert!"}
-    }},
-    {HIGH_WARNING, {
-      {ENGLISH, "Value is approaching the maximum limit!"},
-      {GERMAN, "Wert nähert sich dem Höchstwert!"}
-    }},
-    {NORMAL, {
-      {ENGLISH, "Value is within the normal range."},
-      {GERMAN, "Wert liegt im normalen Bereich."}
+    {"Charge Rate out of range!", {
+      {ENGLISH, "Charge Rate out of range!"},
+      {GERMAN, "Laderate außerhalb des zulässigen Bereichs!"}
     }}
   };
-  return errorMessages[breachType][currentLanguage].c_str();
+  return errorMessages[errorKey][currentLanguage].c_str();
 }
-BreachType classifyRangeBreach(float value, const ParameterRange& range) {
-  if (value < range.min) {
-    return range.lowBreach;
+ 
+bool checkInRange(float value, float min, float max, const char* errorKey) {
+  if (value < min || value > max) { 
+    cout << getErrorMessage(errorKey); // Print error message based on current language
+    return false;
   }
-  if (value > range.max) {
-    return range.highBreach;
-  }
-  
-  // Define warning thresholds
-  const float warningThreshold = 5.0;
-
-  // Check for warnings
-  if (value < range.min + warningThreshold) {
-    return LOW_WARNING;
-  }
-  if (value > range.max - warningThreshold) {
-    return HIGH_WARNING;
-  }
-  
-  return NORMAL;
+  return true;
 }
-
-void printBreachMessage(BreachType breachType) {
-  if (breachType != NORMAL) {
-    cout << getErrorMessage(breachType) << endl;
-  }
-}
-
+ 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  BreachType temperatureBreach = classifyRangeBreach(temperature, temperatureRange);
-  BreachType socBreach = classifyRangeBreach(soc, socRange);
-  BreachType chargeRateBreach = classifyRangeBreach(chargeRate, chargeRateRange);
-
-  printBreachMessage(temperatureBreach);
-  printBreachMessage(socBreach);
-  printBreachMessage(chargeRateBreach);
-
-  return temperatureBreach == NORMAL && socBreach == NORMAL && chargeRateBreach == NORMAL;
+  bool temperatureIsOk = checkInRange(temperature, 0, 45, "Temperature out of range!");
+  bool socIsOk = checkInRange(soc, 20, 80, "State of Charge out of range!");
+  bool chargeRateIsOk = checkInRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
+  return temperatureIsOk && socIsOk && chargeRateIsOk; 
 }
