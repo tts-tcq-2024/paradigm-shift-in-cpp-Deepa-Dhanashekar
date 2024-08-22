86739903adcb7677f79f685c50d67256d8de4685
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-08-22 10:55:54 +0530

    Update battery_check.h

--- battery_check.h
+++ battery_check.h
@@ -1,15 +1,34 @@
 #ifndef BATTERY_CHECK_H
 #define BATTERY_CHECK_H
  
-#include <map>
 #include <string>
- 
-enum Language { ENGLISH, GERMAN };
- 
-extern Language currentLanguage;
- 
-const char* getErrorMessage(const char* errorKey);
-bool checkInRange(float value, float min, float max, const char* errorKey);
+
+enum BreachStatus {
+    NO_BREACH,
+    LOW_BREACH,
+    HIGH_BREACH
+};
+
+enum WarningStatus {
+    NO_WARNING,
+    LOW_WARNING,
+    HIGH_WARNING
+};
+
+struct ParameterRange {
+    float lowBreach;
+    float lowWarning;
+    float highWarning;
+    float highBreach;
+};
+
+BreachStatus getBreachStatus(float value, const ParameterRange& range);
+WarningStatus getWarningStatus(float value, const ParameterRange& range);
+std::string statusToMessageTranslation(const std::string& parameter, BreachStatus breachStatus, WarningStatus warningStatus);
+
+bool checkTemperature(float temperature, BreachStatus& breachStatus, WarningStatus& warningStatus);
+bool checkSoc(float soc, BreachStatus& breachStatus, WarningStatus& warningStatus);
+bool checkChargeRate(float chargeRate, BreachStatus& breachStatus, WarningStatus& warningStatus);
 bool batteryIsOk(float temperature, float soc, float chargeRate);
- 
+
 #endif // BATTERY_CHECK_H
